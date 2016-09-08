ab9da2911749e53af770577183231f7522d836e0
Luis Lloret <luislloret@gmail.com>
2016-09-08 18:05:24 +0100

    Updated and added filter_sweep

--- osc_control/osc.py
+++ osc_control/osc.py
@@ -1,3 +1,25 @@
-﻿import sys
+﻿import argparse
+import random
+import time
 
-print("Hi")
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip", default="127.0.0.1",
+      help="The ip of the OSC server")
+  parser.add_argument("--port", type=int, default=57110,
+      help="The port the OSC server is listening on")
+  args = parser.parse_args()
+
+  client = udp_client.UDPClient(args.ip, args.port)
+
+  for x in range(10):
+    print("Sending message")
+    msg = osc_message_builder.OscMessageBuilder(address = "/status")
+    msg.add_arg(random.random())
+    msg = msg.build()
+    client.send(msg)
+    time.sleep(1)
