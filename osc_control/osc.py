64390134ee64477be6364c8d80b29ba96f71ed1e
Luis <luislloret@gmail.com>
2017-03-03 23:39:05 +0000

    Better test for osmid o2m

--- osc_control/osc.py
+++ osc_control/osc.py
@@ -1,5 +1,4 @@
 ﻿import argparse
-import random
 import time
 
 from pythonosc import osc_message_builder
@@ -7,19 +6,28 @@ from pythonosc import udp_client
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--ip", default="127.0.0.1",
-      help="The ip of the OSC server")
-  parser.add_argument("--port", type=int, default=57110,
-      help="The port the OSC server is listening on")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default="127.0.0.1",
+                        help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=57200,
+                        help="The port the OSC server is listening on")
+    args = parser.parse_args()
 
-  client = udp_client.UDPClient(args.ip, args.port)
+    client = udp_client.UDPClient(args.ip, args.port)
 
-  for x in range(10):
+
     print("Sending message")
-    msg = osc_message_builder.OscMessageBuilder(address = "/status")
-    msg.add_arg(random.random())
+    msg = osc_message_builder.OscMessageBuilder(address="/*/note_on")
+    msg.add_arg(1)
+    msg.add_arg(35)
+    msg.add_arg(60)
     msg = msg.build()
     client.send(msg)
-    time.sleep(1)
+    time.sleep(3)
+
+    msg = osc_message_builder.OscMessageBuilder(address="/*/note_off")
+    msg.add_arg(1)
+    msg.add_arg(35)
+    msg.add_arg(60)
+    msg = msg.build()
+    client.send(msg)
